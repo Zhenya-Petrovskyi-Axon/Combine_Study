45d076c7384d1bebc6c23f985f1b484683cd780f
Ievgen Petrovskiy <epetrovskyi@axon.dev>
2021-11-14 20:52:47 +0200

    [FIX]

--- ExampleApp-Signup/ExampleApp-Signup/SignUpVM.swift
+++ ExampleApp-Signup/ExampleApp-Signup/SignUpVM.swift
@@ -8,6 +8,15 @@
 import Combine
 import UIKit
 
+// SIGN UP FORM RULES
+// - email address must be valid (contain @ and .)
+// - password must be at least 8 characters
+// - password cannot be "password"
+// - password confirmation must match
+// - must agree to terms
+// - BONUS: color email field red when invalid, password confirmation field red when it doesn't match the password
+// - BONUS: email address must remove spaces, lowercased
+
 class SignUpVM {
     // Subjects
     @Published
--- ExampleApp-Signup/ExampleApp-Signup/ViewController.swift
+++ ExampleApp-Signup/ExampleApp-Signup/ViewController.swift
@@ -8,15 +8,6 @@
 import UIKit
 import Combine
 
-// SIGN UP FORM RULES
-// - email address must be valid (contain @ and .)
-// - password must be at least 8 characters
-// - password cannot be "password"
-// - password confirmation must match
-// - must agree to terms
-// - BONUS: color email field red when invalid, password confirmation field red when it doesn't match the password
-// - BONUS: email address must remove spaces, lowercased
-
 class ViewController: UITableViewController {
 
     // MARK: - Outlets
